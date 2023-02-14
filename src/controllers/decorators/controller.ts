4ae9b613c99dbb12928bea5bc48cfe73a9572a46
zysable <82249479+zysable@users.noreply.github.com>
2023-02-13 22:33:41 -0800

    updated

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -27,8 +27,10 @@ export function controller(routePrefix: string) {
       const routeHandler = target.prototype[key]
       const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key)
       const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key)
-      const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || []
-      const requiredBodyProps = Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || []
+      const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key)
+        || []
+      const requiredBodyProps = Reflect.getMetadata(MetadataKeys.validator, target.prototype, key)
+        || []
       const validator = bodyValidators(requiredBodyProps)
       if (path) {
         router[method](`${routePrefix}${path}`, ...middlewares, validator, routeHandler)
